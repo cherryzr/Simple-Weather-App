353899fd56eb7df79c2620b431755c27f57e4f4f
PHOENIXX - 3 <avinashdci@gmail.com>
2024-06-12 06:08:18 +0530

    first commit for weather app

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Weather App
+
+## Description
+
+This is a simple weather application that allows users to search for a location and retrieve real-time weather information, including temperature, description, humidity and wind speed for the searched location.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Fetch API
+- Google Maps Geocoding API
+- Font Awesome Icons
+
+## How to Use
+
+1. Enter the name of the location you want to search for in the input field.
+2. Click the search button or press Enter.
+3. The app will display the current weather information for the searched location, including temperature, description, humidity, wind speed.
+
+## Setup Instructions
+
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in your web browser.
+
+## Credits
+
+- Weather icons provided by [Font Awesome](https://fontawesome.com/).
+- Weather data retrieved from [OpenWeatherMap API](https://openweathermap.org/).
+- GPT
---
+++ app.js
@@ -0,0 +1,96 @@
+// Selecting DOM elements
+const container = document.querySelector(".container");
+const searchButton = document.querySelector(".search-box button");
+const searchInput = document.querySelector(".search-box input");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+// Function to fetch weather data
+function fetchWeatherData(city) {
+  // API Key for OpenWeatherMap API
+  const APIKey = "2f200026ae5c1d68dd0c52ed2e868e31";
+
+  // Fetching weather data from OpenWeatherMap API
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+  )
+    .then((response) => response.json()) // Parsing response as JSON
+    .then((json) => {
+      // If city not found, show 404 error message
+      if (json.cod === "404") {
+        container.style.height = "400px";
+        weatherBox.style.display = "none";
+        weatherDetails.style.display = "none";
+        error404.style.display = "block";
+        error404.classList.add("fadeIn");
+        return;
+      }
+
+      // Hide 404 error message if city found
+      error404.style.display = "none";
+      error404.classList.remove("fadeIn");
+
+      // Selecting DOM elements for weather information
+      const image = document.querySelector(".weather-box img");
+      const temperature = document.querySelector(".weather-box .temperature");
+      const description = document.querySelector(".weather-box .description");
+      const humidity = document.querySelector(
+        ".weather-details .humidity span"
+      );
+      const wind = document.querySelector(".weather-details .wind span");
+
+      // Setting weather icon based on weather condition
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+        case "Rain":
+          image.src = "images/rain.png";
+          break;
+        case "Snow":
+          image.src = "images/snow.png";
+          break;
+        case "Clouds":
+          image.src = "images/cloud.png";
+          break;
+        case "Haze":
+          image.src = "images/mist.png";
+          break;
+        default:
+          image.src = "";
+      }
+
+      // Setting temperature, description, humidity, and wind speed
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+      // Displaying weather information
+      weatherBox.style.display = "";
+      weatherDetails.style.display = "";
+      weatherBox.classList.add("fadeIn");
+      weatherDetails.classList.add("fadeIn");
+      container.style.height = "590px";
+    });
+}
+
+// Adding click event listener to search button
+searchButton.addEventListener("click", () => {
+  const city = searchInput.value.trim();
+  if (city !== "") {
+    fetchWeatherData(city);
+  }
+});
+
+// Adding key press event listener to input field
+searchInput.addEventListener("keypress", (event) => {
+  // Check if Enter key is pressed (key code 13)
+  if (event.keyCode === 13) {
+    const city = searchInput.value.trim();
+    if (city !== "") {
+      fetchWeatherData(city);
+    }
+  }
+});
